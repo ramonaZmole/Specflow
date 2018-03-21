c8f1bf54a6187ccf7e92606ee34707be9efa688c
Ramona Zmole <ramona.zmole@gmail.com>
2018-03-21 21:02:45 +0200

    Add Cart feature file

---
+++ SpecflowSetup/SpecflowSetup/CartFeature.feature
@@ -0,0 +1,9 @@
+﻿Feature: CartFeature
+	In order to place order
+	As a user
+	I want to be able to make changes in cart
+
+Scenario: Remove product from cart
+	Given the user is in cart
+	When the user removes the product from cart
+	Then the cart gets empty
---
+++ SpecflowSetup/SpecflowSetup/CartFeatureSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecflowSetup
+{
+    [Binding]
+    public class CartFeatureSteps
+    {
+        [Given]
+        public void GivenTheUserIsInCart()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When]
+        public void WhenTheUserRemovesTheProductFromCart()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then]
+        public void ThenTheCartGetsEmpty()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
